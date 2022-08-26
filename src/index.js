0d1b7122608dbb6deee7a0df0526285a385739ac
Quadatz <quadath@gmail.com>
2022-08-26 14:36:45 +0300

    redux createStore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import './styles.css'
-import { createStore } from './createStore'
+import { createStore } from 'redux'
 import { rootReducer } from './redux/rootReducer'
 
-const store = createStore(rootReducer, 0)
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
 
+const store = createStore(rootReducer, 0)
 window.store = store;
 
-store
+addBtn.addEventListener('click', () => {
+    store.dispatch({ type: "INC" })
+})
+
+subBtn.addEventListener('click', () => {
+    store.dispatch({ type: "DEC" })
+})
+
+store.subscribe(() => {
+    const state = store.getState()
+    counter.textContent = state;
+})
+store.dispatch({ type: "START" })
+
+asyncBtn.addEventListener('click', () => {
+    
+})
+
+themeBtn.addEventListener('click', () => {
+    
+})
